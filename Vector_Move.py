289e30f2e77c10b18c02bad0434578f040b2a59d
x <felimoles@gmail.com>
2019-12-20 16:57:14 -0300

    add vector 3

--- Vector_Move.py
+++ Vector_Move.py
@@ -23,3 +23,33 @@ class VectorMove(Scene):
 
         #self.play(ShowCreation(vector2))
         #self.play(ShowCreation(v2txt))
+        
+class Vector3(Scene):
+    def construct(self):
+        v1 = np.array([3,0,0])
+        v2 = np.array([-3,0,0])
+        v3 = v1+v2
+        vector1 = Vector(v1)
+        vector2 = Vector(v2)
+        vector3 = Vector(v3)
+       # v1txt = TextMobject("A").next_to(vector1,DOWN)
+        #v2txt = TextMobject("-A").next_to(vector2,LEFT)
+        v3txt = TextMobject("X+Y").next_to(vector3,RIGHT)
+        v1txt = TextMobject("A")
+        v2txt = TextMobject("-A")
+        v1txt.shift(UR)
+        v2txt.shift(UL)
+        #self.play(ShowCreation(v1txt))
+        self.play(ShowCreation(vector1),Write(v1txt))
+        self.wait()
+        self.play(Transform(vector1,vector2),Transform(v1txt,v2txt))
+        #self.play(Transform(v1txt,v2txt))
+        #self.play(ShowCreation(vector3),Write(v3txt))
+        
+
+        #vgroup_ = VGroup(vector1,v1txt,vector2,v2txt,vector3,v3txt)
+        #self.play(ApplyMethod(vgroup_.to_edge, LEFT, {"buff": 1.6}))
+        #self.play(ShowCreation(v1txt))
+
+        #self.play(ShowCreation(vector2))
+        #self.play(ShowCreation(v2txt))
