e6b371a96228eb4f4e4b4e20d511e85303d4ee90
x <felimoles@gmail.com>
2020-01-02 12:58:31 -0300

    add 3D View

--- Vector_Move.py
+++ Vector_Move.py
@@ -53,3 +53,54 @@ class Vector3(Scene):
 
         #self.play(ShowCreation(vector2))
         #self.play(ShowCreation(v2txt))
+class View3D(ThreeDScene):
+   
+    def construct(self):
+        axes = ThreeDAxes()
+        v1 = np.array([3,-3,0])
+        v2 = np.array([3,-3,3])
+        v3 = np.array([0,-3,0])
+
+        #ln1 = DashedLine(np.array([3,0,0]),np.array([3,-3,0]),buff=0.1)
+        ln0 = self.drawline([0,0,0],[0,-3,0])
+        ln1 = self.drawline([0,-3,0],[3,-3,0])
+        ln2 = self.drawline([3,-3,0],[3,0,0])
+        ln3 = self.drawline([3,0,0],[0,0,0])
+        
+        #top
+        ln0t = self.drawline([0,0,3],[0,-3,3])
+        ln1t = self.drawline([0,-3,3],[3,-3,3])
+        ln2t = self.drawline([3,-3,3],[3,0,3])
+        ln3t = self.drawline([3,0,3],[0,0,3])
+
+        vector1 = Vector(v1,color="red")
+        vector2 = Vector(v2,color="red")
+        #line
+        ln0r = self.drawline([0,0,0],[0,0,3])
+        ln1r = self.drawline([0,-3,0],[0,-3,3])
+        ln2r = self.drawline([3,0,0],[3,0,3])
+        ln3r = self.drawline([3,-3,0],[3,-3,3])
+
+        self.play(ShowCreation(axes))
+        self.play(ShowCreation(vector1))
+        self.play(ShowCreation(ln0))
+        self.play(ShowCreation(ln1))
+        self.play(ShowCreation(ln2))
+        self.play(ShowCreation(ln3))
+        self.wait()
+        self.move_camera(phi=65*DEGREES,theta=-75*DEGREES)
+        self.wait()
+        self.play(ShowCreation(ln0t))
+        self.play(ShowCreation(ln1t))
+        self.play(ShowCreation(ln2t))
+        self.play(ShowCreation(ln3t))
+        self.play(ShowCreation(ln0r),ShowCreation(ln1r),ShowCreation(ln2r),ShowCreation(ln3r))
+        self.play(Transform(vector1,vector2))
+
+
+
+    def drawline(self,initial,endline):
+        #print(initial,endline)
+        ln = DashedLine(np.array(initial),np.array(endline),buff=0.1)
+        ln.set_color(YELLOW)
+        return ln
